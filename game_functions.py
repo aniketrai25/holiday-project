7a40d0ba972c96c21eab3cc1ea0c281ff66023c3
aniketrai25 <steamzeus25@gmail.com>
2017-07-03 15:05:07 +1200

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -35,13 +35,25 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats, play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button, ship, 
+                              aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, 
+                      bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play"""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game statistics
+        stats.reset_stats()
         stats.game_active = True
-            
+        
+        # Empty the list of aliens and bullets
+        aliens.empty()
+        bullets.empty()
+        
+        # Create a new fleet and center the ship
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet, if limit not reached yet"""
     # Create a new bullet, add to bullets group
