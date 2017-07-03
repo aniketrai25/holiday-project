8b04e960ebfa5a5d25ba46c0be9f58adea32e0fa
aniketrai25 <steamzeus25@gmail.com>
2017-07-03 15:10:08 +1200

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -6,11 +6,9 @@ class GameStats():
         self.ai_settings = ai_settings
         self.reset_stats()
 
-        # Start the game in an active state
-        self.game_active = True
+        # Start the game in an inactive state
+        self.game_active = False
 
     def reset_stats(self):
         """Initialise statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
-
-        
