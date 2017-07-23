fd0c7dcebcd5f4a76bd4dc1f86095a9f91ae5ecc
aniketrai25 <steamzeus25@gmail.com>
2017-07-23 15:29:57 +1200

    FINISHED PROJECT
    DONE

--- game_functions.py
+++ game_functions.py
@@ -24,7 +24,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -35,16 +35,26 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, 
-                              aliens, bullets, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, sb, play_button,
+                              ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, 
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, 
                       bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play"""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
+        
         # Reset the game statistics
-        stats.reset_stats()
+        ai_settings.intialise_dynamic_settings()
         stats.game_active = True
+        
+        # Hide the mouse cursor
+        pygame.mouse.set_visible(False)
+
+        # Reset the scoreboard images
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
         
         # Empty the list of aliens and bullets
         aliens.empty()
@@ -53,6 +63,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
         # Create a new fleet and center the ship
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
+
         
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet, if limit not reached yet"""
@@ -61,7 +72,7 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         new_bullet = Bullet(ai_settings, screen, ship)
         bullets.add(new_bullet)
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """Update images on the screen, and flip to the new screen"""
     # Redraw the screen, each pass through the loop
     screen.fill(ai_settings.bg_colour)
@@ -71,6 +82,9 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    # Draw the score information
+    sb.show_score()
     
     # Draw the play button if the game in inactive
     if not stats.game_active:
@@ -79,7 +93,7 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
     # Make the most recently drawn screen visible
     pygame.display.flip()
     
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Update position of bullets, and get rid of old bullets"""
     # Update bullet positions
     bullets.update()
@@ -89,16 +103,27 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
             
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
             
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Respond to bullet-alien collisions"""
     # Remove any bullets and aliens that have collided
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if collisions:
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points
+            sb.prep_score()
+        check_high_score(stats, sb)
     
     if len(aliens) == 0:
-        # Destroy existing bullets, and create new fleet
+        # Destroy existing bullets, and create new fleet and start the new level
         bullets.empty()
+        ai_settings.increase_speed()
+
+        # Increase the level
+        stats.level += 1
+        sb.prep_level()
         create_fleet(ai_settings, screen, ship, aliens)
     
 def check_fleet_edges(ai_settings, aliens):
@@ -191,3 +216,10 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+def check_high_score(stats, sb):
+    """Check to see if there is a new high score"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
+        
--- settings.py
+++ settings.py
@@ -2,6 +2,17 @@ import pygame
 
 class Settings():
     """A class to store all settings for a game"""
+    def intialise_dynamic_settings(self):
+        """Initialise settings that change throughout the game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1.5
+
+        # fleet_direction of 1 is right; -1 is left
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
 
     def __init__(self):
         """Initialize the game settings"""
@@ -12,7 +23,7 @@ class Settings():
 
         # Ship settings
         self.ship_speed_factor = 1.5
-        self.ship_limit = 5
+        self.ship_limit = 0
 
         # Bullet settings
         self.bullet_speed_factor = 3
@@ -22,9 +33,25 @@ class Settings():
         self.bullets_allowed = 5
 
         # Alien Settings
-        self.alien_speed_factor = 1.2
-        self.fleet_drop_speed = 8
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
         # fleet_direction of 1 is right; -1 is left
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.intialise_dynamic_settings()
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values"""
+        self.ship_speed_factor *=  self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
         
         
